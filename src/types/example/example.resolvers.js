9b93a77695eb34133a0e32c696d8a4e1cd05f5ac
Jayant Pareek <jayant@springboard.com>
2019-07-25 12:24:03 +0530

    GraphQL Training: Add solutions for Interface exercise
    Create Pet interface to query all pets
    
    Signed-off-by: Jayant Pareek <jayant@springboard.com>

--- src/types/example/example.resolvers.js
+++ src/types/example/example.resolvers.js
@@ -1,3 +1,20 @@
+// {
+//   myDog {
+//     name
+//     age
+//   }
+  
+//   myCat {
+//     name
+//     age
+//   }
+  
+//   myParrot {
+//     name
+//     age
+//   }
+// }
+
 const persons = [
   {
   name: 'Jayant',
@@ -9,22 +26,67 @@ const persons = [
   }
 ];
 
-const cat = [{
+const cat = {
   name: 'Katie',
-  age: 2
-}];
+  age: 2,
+  type: 'Cat',
+  furColor: 'Brown'
+};
+
+const dog = {
+  name: 'Becky',
+  age: 5,
+  type: 'Dog',
+  breed: 'Husky'
+};
+
+const parrot = {
+  name: 'Packkot',
+  age: 1,
+  type: 'Parrot',
+  color: 'green-yellow',
+};
+
+const pets = [
+  cat,
+  dog,
+  parrot
+];
 
 const catResolver = (root, args, ctx) => {
   return cat;
+}
+
+const dogResolver = (root, args, ctx) => {
+  return dog;
 }
 
 const personResolver = (root, args, ctx) => {
   return persons;
 }
 
+const parrotResolver = (root, args, ctx) => {
+  return parrot;
+}
+
+const petResolver = (root, args, ctx) => {
+  return pets;
+}
+
+
+
+
 export default {
   Query: {
     myCat: catResolver,
+    myDog: dogResolver,
+    myParrot: parrotResolver,
+    myPets: petResolver,
     persons: personResolver
   },
+  Pet: {
+    __resolveType(pet) {
+      return pet.type
+    }
+  }
 }
